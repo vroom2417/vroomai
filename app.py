da61635c2c52077a2e782df4f00d751f3dd1983f
Varun Ashok Agarwal <48707163+vroom2417@users.noreply.github.com>
2026-03-23 21:21:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="VroomAI", page_icon="🚀")
 
 st.title("Welcome to VroomAI")
-st.write("Portfolio of AI projects and learning\n New projects coming soon")
+st.write("Portfolio of AI projects and learning \n New projects coming soon")
 
 st.image("logo.png")
 
